6481f9a89be390d9173d678d60f21514064dc2bd
Zhengnan Lee <zhengnanlee@hotmail.com>
2013-08-09 22:09:24 +0800

    Update nCr.cpp

--- Combinations/nCr.cpp
+++ Combinations/nCr.cpp
@@ -22,3 +22,25 @@ int getcombination(int n, int m)
 	}
 	return c[m];
 }
+//////no.3 for C(a+b,a)
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define ULL unsigned long long int
+
+int main()
+{
+	ULL a, b;
+	while (cin >> a >> b && (a || b))
+	{
+		if (b > a) swap(a, b);
+		ULL s = 1, i, j;
+		for (i = a + 1, j = 1; i <= a + b; i++, j++)
+		{
+			s = s * i / j;
+		}
+		cout << s << endl;
+	}
+}
